b274b6edc11886b521a75acba0747184878c9b50
Amigo20057 <denrizuk@gmail.com>
2024-09-24 18:04:21 +0300

    header - implementation

--- local/src/App.tsx
+++ local/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
+import { Header } from './components/Header/Header'
 import { Register } from './pages'
 import { Login } from './pages/Auth/Login/Login'
 
 export const App: React.FC = () => {
 	return (
 		<>
+			<Header />
 			<Routes>
 				<Route path='/register' element={<Register />} />
 				<Route path='/login' element={<Login />} />
