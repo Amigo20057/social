a416209cfe7e59a6831c7d9833bd36fd82f74d70
Amigo20057 <denrizuk@gmail.com>
2025-06-01 18:18:44 +0300

    fix bug

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,43 +1,43 @@
-import { createUserDto } from '@/user/dto/createUserDto'
-import { UserService } from '@/user/user.service'
-import { Request, Response, Router } from 'express'
-import { AuthService } from './auth.service'
+import { createUserDto } from "@/user/dto/createUserDto";
+import { UserService } from "@/user/user.service";
+import { Request, Response, Router } from "express";
+import { AuthService } from "./auth.service";
 
-const route = Router()
-const userService = new UserService()
-const authService = new AuthService()
+const route = Router();
+const userService = new UserService();
+const authService = new AuthService();
 
-route.post('/register', async (req: Request, res: Response) => {
+route.post("/register", async (req: Request, res: Response) => {
 	try {
-		const validate = createUserDto.safeParse(req.body)
+		const validate = createUserDto.safeParse(req.body);
 
 		if (!validate.success) {
-			return res.status(400).json({ message: validate.error })
+			return res.status(400).json({ message: validate.error });
 		}
 
-		const user = await userService.createUser(req.body)
+		const user = await authService.register(req.body);
 
-		res.status(200).json(user)
+		res.status(200).json(user);
 	} catch (err: unknown) {
 		if (err instanceof Error) {
-			res.status(500).json({ error: err.message })
+			res.status(500).json({ error: err.message });
 		} else {
-			res.status(500).json({ error: 'An unknown error occurred' })
+			res.status(500).json({ error: "An unknown error occurred" });
 		}
 	}
-})
+});
 
-route.post('/login', async (req: Request, res: Response) => {
+route.post("/login", async (req: Request, res: Response) => {
 	try {
-		const user = await authService.login(req.body)
-		res.status(200).json(user)
+		const user = await authService.login(req.body);
+		res.status(200).json(user);
 	} catch (err: unknown) {
 		if (err instanceof Error) {
-			res.status(500).json({ error: err.message })
+			res.status(500).json({ error: err.message });
 		} else {
-			res.status(500).json({ error: 'An unknown error occurred' })
+			res.status(500).json({ error: "An unknown error occurred" });
 		}
 	}
-})
+});
 
-export const authRouter = route
+export const authRouter = route;
