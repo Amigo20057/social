7d8c8ad368ef3fd7aef008b80989cceab9ee9787
Amigo20057 <denrizuk@gmail.com>
2025-06-01 17:58:32 +0300

    fix auth

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,24 +1,38 @@
-import { UserService } from '@/user/user.service'
-import * as bcrypt from 'bcrypt'
-import jwt from 'jsonwebtoken'
-import { ILogin } from './auth.types'
+import { UserService } from "@/user/user.service";
+import { IUser } from "@/user/user.types";
+import * as bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import { ILogin } from "./auth.types";
 
 export class AuthService extends UserService {
 	async login(dto: ILogin): Promise<ILogin & { token: string }> {
-		const user = await this.findUserByEmail(dto.email)
+		const user = await this.findUserByEmail(dto.email);
 		if (!user) {
-			throw new Error('User does not exist')
+			throw new Error("User does not exist");
 		}
 
-		const validatePassword = await bcrypt.compare(dto.password, user.password)
+		const validatePassword = await bcrypt.compare(dto.password, user.password);
 		if (!validatePassword) {
-			throw new Error('Incorrect login or password')
+			throw new Error("Incorrect login or password");
 		}
 
-		const token = jwt.sign({ _id: user.id }, process.env.SECRET || '', {
-			expiresIn: '30d',
-		})
+		const token = jwt.sign({ _id: user.id }, process.env.SECRET || "", {
+			expiresIn: "30d",
+		});
 
-		return { ...user, token }
+		return { ...user, token };
+	}
+
+	async register(dto: IUser): Promise<IUser & { token: string }> {
+		const existsUser = await this.findUserByEmail(dto.email);
+		if (existsUser) {
+			throw new Error("User already exists");
+		}
+		dto.password = await this.hashPassword(dto.password);
+		const user = await this.createUser(dto);
+		const token = jwt.sign({ _id: user.id }, process.env.SECRET || "", {
+			expiresIn: "30d",
+		});
+		return { ...user, token } as IUser & { token: string };
 	}
 }
--- server/src/user/user.service.ts
+++ server/src/user/user.service.ts
@@ -1,42 +1,52 @@
-import { PrismaClient } from '@prisma/client'
-import * as bcrypt from 'bcrypt'
-import jwt from 'jsonwebtoken'
-import { IUser } from './user.types'
+import { PrismaClient } from "@prisma/client";
+import * as bcrypt from "bcrypt";
+import { IUser } from "./user.types";
 
 export class UserService {
-	protected prisma = new PrismaClient()
+	protected prisma = new PrismaClient();
 
 	async findUserByEmail(email: string) {
-		return await this.prisma.user.findUnique({ where: { email } })
+		return await this.prisma.user.findUnique({ where: { email } });
 	}
 
 	async hashPassword(password: string) {
-		return await bcrypt.hash(password, 10)
+		return await bcrypt.hash(password, 10);
 	}
 
-	async createUser(dto: IUser): Promise<IUser & { token: string }> {
-		const existUser = await this.findUserByEmail(dto.email)
-		if (existUser) {
-			throw new Error('User already exists')
-		}
-
-		dto.password = await this.hashPassword(dto.password)
-
-		const user = await this.prisma.user.create({
+	async createUser(dto: IUser): Promise<IUser & { id: string }> {
+		return await this.prisma.user.create({
 			data: {
 				name: dto.name,
 				email: dto.email,
 				password: dto.password,
 				avatar: dto.avatar,
 			},
-		})
-
-		const token = jwt.sign({ _id: user.id }, process.env.SECRET || '', {
-			expiresIn: '30d',
-		})
-
-		return { ...user, token } as IUser & { token: string }
+		});
 	}
+
+	// async createUser(dto: IUser): Promise<IUser & { token: string }> {
+	// 	const existUser = await this.findUserByEmail(dto.email);
+	// 	if (existUser) {
+	// 		throw new Error("User already exists");
+	// 	}
+
+	// 	dto.password = await this.hashPassword(dto.password);
+
+	// 	const user = await this.prisma.user.create({
+	// 		data: {
+	// 			name: dto.name,
+	// 			email: dto.email,
+	// 			password: dto.password,
+	// 			avatar: dto.avatar,
+	// 		},
+	// 	});
+
+	// 	const token = jwt.sign({ _id: user.id }, process.env.SECRET || "", {
+	// 		expiresIn: "30d",
+	// 	});
+
+	// 	return { ...user, token } as IUser & { token: string };
+	// }
 
 	async updateUser(
 		userId: string,
@@ -45,15 +55,15 @@ export class UserService {
 		return await this.prisma.user.update({
 			where: { id: userId },
 			data: updates,
-		})
+		});
 	}
 
 	async getMe(userId: string) {
-		const user = await this.prisma.user.findUnique({ where: { id: userId } })
+		const user = await this.prisma.user.findUnique({ where: { id: userId } });
 		if (!user) {
-			throw new Error('User does not exist')
+			throw new Error("User does not exist");
 		}
 
-		return user
+		return user;
 	}
 }
